daaa9c977a0c01fff978540dd1a3f90dabe916b3
Erik Bonifaz <erikbo9@hotmail.com>
2024-12-02 14:29:02 -0600

    scroll script change

--- public/scripts/scroll-navbar.js
+++ public/scripts/scroll-navbar.js
@@ -6,9 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const sectionTop = whyDubaiSection.getBoundingClientRect().top + window.scrollY;
 
     if (window.scrollY >= sectionTop) {
+      navbar.classList.remove('hidden');
+      navbar.classList.remove('flex');
       navbar.classList.add('md:flex');
     } else {
       navbar.classList.remove('md:flex');
+      navbar.classList.add('flex');
       navbar.classList.add('hidden');
     }
   };
